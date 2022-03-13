90dfedbc81e6678fa8db5d20348cbf2e8ee0b605
smithgrizzz <smithgrizzly9@gmail.com>
2022-03-13 17:35:21 +0700

    Mengubah format tab

--- login.php
+++ login.php
@@ -2,41 +2,41 @@
 session_start();
 
 if ( isset($_COOKIE["login"]) ) {
-    if( $_COOKIE["login"] == 'true' ) {
-        $_SESSION["login"] = true;
-    }
+  if( $_COOKIE["login"] == 'true' ) {
+    $_SESSION["login"] = true;
+  }
 }
 
 if( isset($_SESSION["login"]) ) {
-    header("Location: admin/index.php");
-    exit;
+  header("Location: admin/index.php");
+  exit;
 }
 
 include 'function/functions.php';
 
 if( isset($_POST["login"]) ) {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    
-    $result = mysqli_query($conn, "SELECT * FROM admin WHERE username = '$username'");
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+  
+  $result = mysqli_query($conn, "SELECT * FROM admin WHERE username = '$username'");
 
-    if( mysqli_num_rows($result) === 1 ) {
-        $row = mysqli_fetch_assoc($result);
-        if( password_verify($password, $row["password"]) ) {
-            $_SESSION["login"] = true;
+  if( mysqli_num_rows($result) === 1 ) {
+    $row = mysqli_fetch_assoc($result);
+    if( password_verify($password, $row["password"]) ) {
+      $_SESSION["login"] = true;
 
-            if( isset($_POST["remember"]) ) {
-                setcookie("login", true, time()+60);
-            }
+      if( isset($_POST["remember"]) ) {
+        setcookie("login", true, time()+60);
+      }
 
-            $_SESSION["username"] = $username;
-            header("Location: admin/index.php");
-            exit;
-        }else {
-            header("Location: index.php?pesan=gagal_u");
-        }
+      $_SESSION["username"] = $username;
+      header("Location: admin/index.php");
+      exit;
     }else {
-        header("Location: index.php?pesan=gagal_p");
+      header("Location: index.php?pesan=gagal_u");
     }
+  }else {
+    header("Location: index.php?pesan=gagal_p");
+  }
 
 }
