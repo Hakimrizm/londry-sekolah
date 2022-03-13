5b1891763e5be1cafaf89b994baae037fb84216c
smithgrizzz <smithgrizzly9@gmail.com>
2022-03-13 15:13:11 +0700

    Memodular kan kode ajax, agar bisa dipakai berulang-ulang

--- public/js/search.js
+++ public/js/search.js
@@ -2,16 +2,20 @@ const keyword = document.getElementById("keyword");
 const search = document.querySelector(".search");
 
 keyword.addEventListener("keyup", function () {
+  cari("GET", `../ajax/pelanggan.php?keyword=${keyword.value}`, search);
+});
+
+const cari = function (method, url, container) {
   // Buat Object Ajax
   const xhr = new XMLHttpRequest();
 
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
-      search.innerHTML = xhr.responseText;
+      container.innerHTML = xhr.responseText;
     }
   };
 
   // Eksekusi Ajax
-  xhr.open("GET", `../ajax/pelanggan.php?keyword=${keyword.value}`, true);
+  xhr.open(method, url, true);
   xhr.send();
-});
+};
