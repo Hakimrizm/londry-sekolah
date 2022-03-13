02e6edd36a71262b70a89c628cd2a1200da3ef09
smithgrizzz <smithgrizzly9@gmail.com>
2022-03-13 15:15:13 +0700

    Menambahkan komentar supaya tidak pusing

--- public/js/search.js
+++ public/js/search.js
@@ -2,6 +2,7 @@ const keyword = document.getElementById("keyword");
 const search = document.querySelector(".search");
 
 keyword.addEventListener("keyup", function () {
+  // Panggil function ajax yang saya buat
   cari("GET", `../ajax/pelanggan.php?keyword=${keyword.value}`, search);
 });
 
@@ -9,6 +10,7 @@ const cari = function (method, url, container) {
   // Buat Object Ajax
   const xhr = new XMLHttpRequest();
 
+  // Cek jika sudah siap
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
       container.innerHTML = xhr.responseText;
